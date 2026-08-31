5c8519d060862c0ea0d4ac2f19a0caa443613fd5
samir bhusal <bhusalsamir907@gmail.com>
2026-08-30 22:43:44 -0500

    feat: allure report

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"step_def"},
         plugin = {
                 "json:output/cucumber.json",
-                "junit:output/TEST-report.xml"},
+                "junit:output/TEST-report.xml",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true,
         tags = "@TES-001"
 )
